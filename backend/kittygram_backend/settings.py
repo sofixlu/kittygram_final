76b27c8aa8a56a903010867d8973b1713b1fd948
sofixlu <sofia.luky@icloud.com>
2026-04-22 15:35:41 +0300

    fix db host for CI

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -60,8 +60,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432,
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
